226b9ef69c1dc7d0685bc95b3b4b63ab66fa7831
clp102 <clp102@163.com>
2025-06-17 12:40:24 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.106.116.17:21116"];
+pub const RS_PUB_KEY: &str = "bp4cBfysJxEPXf8YqcW3X4dgnxb3f4nbeCx8TrH7kDw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
